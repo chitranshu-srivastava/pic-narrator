2958a771f75c93f2af1afb9a5b4d13cf46713380
chitranshu-srivastava <chitranshus31@gmail.com>
2024-02-29 20:55:44 +0530

    Added webcam to capture image

--- src/client.ts
+++ src/client.ts
@@ -2,45 +2,36 @@ import { pipeline, env } from "@xenova/transformers";
 
 env.allowLocalModels = false;
 
-export function setupCounter(element: HTMLButtonElement) {
-  console.log(import.meta.env.VITE_HUGGINGFACEHUB_API_TOKEN);
+const videoEl = document.querySelector<HTMLVideoElement>('video');
+const canvasEl = document.querySelector<HTMLCanvasElement>('canvas');
 
-  let counter = 0;
-  const setCounter = (count: number) => {
-    counter = count;
-    element.innerHTML = `count is ${counter}`;
-  };
+export async function initiateWebcam() {
+  if (!videoEl) return;
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false,
+  });
+  videoEl.srcObject = stream;
+  videoEl.play();
+}
 
-  element.addEventListener("click", () => setCounter(counter + 1));
-  setCounter(0);
+function drawWebcam() {
+  const ctx = canvasEl?.getContext('2d');
+  if (!videoEl || !canvasEl || !ctx) return '';
+  canvasEl.width = videoEl.videoWidth;
+  canvasEl.height = videoEl.videoHeight;
+  console.log(canvasEl.width, canvasEl.height);
+  ctx.drawImage(videoEl, 0, 0, canvasEl.width, canvasEl.height);
+  const data = canvasEl.toDataURL('image/jpeg');
+  return data;
 }
 
 export async function generateImageCaption() {
-  
   try {
-    const imageInput:any = document.getElementById('image');
-    const file = imageInput.files[0];
-
-
-    const canvas:any = document.getElementById('imageCanvas');
-    const ctx = canvas.getContext('2d');
+    const dataUrl:any = drawWebcam();
 
     const img = new Image();
-      img.onload = function () {
-        // Set canvas size to match the image size
-        canvas.width = img.width;
-        canvas.height = img.height;
-
-        // Draw the image on the canvas
-        ctx.drawImage(img, 0, 0, img.width, img.height);
-        canvas.style.display = 'block';
-
-        // // Get the data URL from the canvas
-        // const dataUrl = canvas.toDataURL();
-
-      };
-
-      img.src = URL.createObjectURL(file);
+    img.src = dataUrl;
 
 
     let captioner = await pipeline(
@@ -48,11 +39,10 @@ export async function generateImageCaption() {
       "Xenova/vit-gpt2-image-captioning"
     );
 
-    // const url = "/Screenshot_20191211-044600_Photos.jpg"
     const output = await captioner(img.src);
 
     console.log(output);
   } catch (error) {
-    console.error("Error---", error);
+    console.error("Error:", error);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,10 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Generate Image Caption</h1>
     <div class="card">
-    <form id="upload-form" enctype="multipart/form-data">
-      <label for="image">Choose an image:</label>
-      <input type="file" id="image" name="image" accept="image/*" required />
+    <video src=""></video>
       <canvas id="imageCanvas" style="display: none;"></canvas>
       <button type="button" onclick="generateImageCaption()" id="upload-btn">Upload Image</button>
-    </form>
+    
     </div>
   </div>
 `
