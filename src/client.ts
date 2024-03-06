9edcade6c29af52eaf2b07e0f4f7581b7adbcd69
chitranshu-srivastava <chitranshus31@gmail.com>
2024-03-06 09:17:48 +0530

    Generating story with hugging face model

--- src/client.ts
+++ src/client.ts
@@ -5,6 +5,8 @@ env.allowLocalModels = false;
 const videoEl = document.querySelector<HTMLVideoElement>('video');
 const canvasEl = document.querySelector<HTMLCanvasElement>('canvas');
 
+
+
 export async function initiateWebcam() {
   if (!videoEl) return;
   const stream = await navigator.mediaDevices.getUserMedia({
@@ -42,7 +44,33 @@ export async function generateImageCaption() {
     const output = await captioner(img.src);
 
     console.log(output);
+    generateStory();
   } catch (error) {
     console.error("Error:", error);
   }
+}
+
+async function generateStory() {
+  const generator = await pipeline('text-generation', 'Xenova/Qwen1.5-0.5B-Chat');
+  
+  // Define the prompt and list of messages
+  const prompt = "Write a short story in 50 words on a quick brown fox jumping over the lazy dog."
+  const messages = [
+      { "role": "system", "content": "You are a story teller" },
+      { "role": "user", "content": prompt }
+  ]
+  
+  // Apply chat template
+  const text:any = generator.tokenizer.apply_chat_template(messages, {
+      tokenize: false,
+      add_generation_prompt: true,
+  });
+  
+  // Generate text
+  const output:any = await generator(text, {
+      max_new_tokens: 128,
+      do_sample: false,
+  });
+  console.log(output[0].generated_text);
+  
 }
